4e66c4d3aa604a9e6c8c8aaac6794361b7ab2a2d
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-26 09:32:52 +0100

    sample2 step defs added

--- src/test/java/Sample2Stepdefs.java
+++ src/test/java/Sample2Stepdefs.java
@@ -1,13 +1,5 @@
-import cucumber.api.PendingException;
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import org.junit.Assert;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.testng.annotations.Test;
 
 /**
